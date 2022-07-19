5b1c2a6cbb6ed19c8873b263380721d52154a6a0
Kirkland Childers <robert.kirkland.childers@gmail.com>
2022-07-19 17:05:49 -0400

    Working on registeration

--- server/controllers/authorization.controller.js
+++ server/controllers/authorization.controller.js
@@ -1,5 +1,30 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+
 exports.registerUser = (req, res) => {
     console.log('Hitting Registration');
+    try{
+        const {
+            userName,
+            email,
+            password,
+            confirm
+        } = req.body;
+        const errors = validateUserRegistration(req.body);
+        if(errors.isValid){
+            const user = new User({
+                userName,
+                email,
+                password : await bcrypt.hash(password, 15)
+            });
+        }
+    }catch(errors){
+        console.log(`Errors trying to register user: ${errors}`);
+        return res.send(JSON.stringify({
+            success : false,
+            errors
+        }));
+    }
     return res.send(JSON.stringify({debug:true}));
 }
 
@@ -8,6 +33,30 @@ exports.loginUser = (req, res) => {
     return res.send(JSON.stringify({debug:true}));
 }
 
-const validateUserInfo = () => {
-    
+const validateUserRegistration = async ({userName, email, password, confirm}) => {
+    let isValid = true;
+    const validations = {};
+    const checkEmail = await User.findOne({email});
+    if(email === ''){
+        isValid = false;
+        validations.email = {userNameRequired : true}
+    }
+    if(checkEmail){
+        isValid = false;
+        validations.email = {...validations.email, emailTaken : true}
+    }
+    if(userName === ''){
+        isValid = false;
+        validations.userName = {userNameRequired : true}
+    }
+    if(password.length < 8){
+        isValid = false;
+        validations.password = {passwordLength : true}
+    }
+    if(confirm !== password){
+        isValid = false;
+        validations.confirm = {matchPassword : true}
+    }
+    validations.isValid = isValid;
+    return validations;
 }
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,7 +1,40 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-
+    userName : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 8
+    },
+    userRecipes : {
+        default : [Schema.Types.ObjectId]
+    },
+    savedRecipes : {
+        default : [Schema.Types.ObjectId]
+    },
+    userLogs : {
+        default : [Schema.Types.ObjectId]
+    },
+    createdAt : {
+        type : Date,
+        required : true,
+        default : new Date(),
+        immutable : true
+    },
+    updatedAt : {
+        type : Date,
+        required : true,
+        default : new Date()
+    }
 });
 
 module.exports = model('users', userSchema);
