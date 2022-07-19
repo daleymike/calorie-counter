08f7e002628bb3bb85716b01f3df66744974ee28
daleymike <98833177+daleymike@users.noreply.github.com>
2022-07-19 16:54:52 -0400

    route structure set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import Test from './components/Test';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Test />
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<div>Login and Reg</div>} />
+        <Route path="/dashboard" element={<div>User Dashboard and Log Form NAV</div>} />
+        <Route path="/recipes/:user_id" element={<div>Display User Recipes NAV</div>} />
+        <Route path="/logs/:user_id" element={<div>Display User Logs NAV</div>}/>
+        <Route path="/recipes/saved/:user_id" element={<div>Display User Saved Recipes NAV</div>} />
+        <Route path="/recipes/create" element={<div>Recipe Form NAV</div>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
