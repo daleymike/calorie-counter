344d5d6341cbbf43b3989a0b6534ca09a531b286
Kirkland Childers <robert.kirkland.childers@gmail.com>
2022-07-20 13:56:57 -0400

    Live validations are active

--- client/src/components/Forms/LoginForm.jsx
+++ client/src/components/Forms/LoginForm.jsx
@@ -7,6 +7,10 @@ const LoginForm = ({className}) => {
         email : '',
         password : ''
     });
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    const validateEmail = () => emailRegex.test(inputs.email);
+    const validatePassword = () => inputs.password.length >= 8;
 
     const handleInput = (e) => {
         const {name, value} = e.target;
@@ -18,24 +22,31 @@ const LoginForm = ({className}) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('Hitting Submit Login');
+        if(validateEmail() && validatePassword()){
+            console.log('Can submit Login Request');
+        }
     }
     
     return (
         <form onSubmit={(e) => handleSubmit(e)} className={className}>
             <h2>Register</h2>
             <div className='my-3'>
-                <label className='float-start' htmlFor="email">Email:</label>
+                <label className='float-start' htmlFor="email">Email:
+                    <span className={validateEmail() ? 'text-success' : 'text-danger'}> Must have valid email address</span>
+                </label>
                 <input className='form-control'
                 onChange={(e) => handleInput(e)} value={inputs.email}
                 type="text" name="email" id="email" />
             </div>
             <div className='my-3'>
-                <label className='float-start' htmlFor="password">Password:</label>
+                <label className='float-start' htmlFor="password">Password:
+                    <span className={validatePassword() ? 'text-success' : 'text-danger'}> Must have valid email address</span>
+                </label>
                 <input className='form-control'
                 onChange={(e) => handleInput(e)} value={inputs.password}
                 type="text" name="password" id="password" />
             </div>
-            <input className='btn btn-dark' type="submit" value="Login" />
+            <input disabled={!validateEmail() && !validatePassword()} className='btn btn-dark' type="submit" value="Login" />
         </form>
     );
 }
--- client/src/components/Forms/RegisterForm.jsx
+++ client/src/components/Forms/RegisterForm.jsx
@@ -9,6 +9,14 @@ const RegisterForm = ({className}) => {
         password : '',
         confirm : ''
     });
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    const validateName = () => inputs.userName !== '';
+    const validateEmail = () => emailRegex.test(inputs.email);
+    const validatePassword = () => inputs.password.length >= 8;
+    const validateConfirm = () => validatePassword() && inputs.confirm === inputs.password;
+
+    const canSubmit = () => validateName() && validateEmail() && validatePassword() && validateConfirm();
 
     const handleInput = (e) => {
         const {name, value} = e.target;
@@ -19,37 +27,49 @@ const RegisterForm = ({className}) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Hitting Submit Login');
+        console.log('Hitting Submit Registration');
+        if(canSubmit()){
+            console.log('Can submit Registration!!');
+            // Registration call here
+        }
     }
     
     return (
         <form onSubmit={(e) => handleSubmit(e)} className={className}>
             <h2>Register</h2>
             <div className='my-3'>
-                <label className='float-start' htmlFor="userName">Name:</label>
+                <label className='float-start' htmlFor="userName">Name:
+                    <span className={validateName() ? 'text-success' : 'text-danger'}>Name is required</span>
+                </label>
                 <input className='form-control'
                 onChange={(e) => handleInput(e)} value={inputs.userName}
                 type="text" name="userName" id="userName" />
             </div>
             <div className='my-3'>
-                <label className='float-start' htmlFor="email">Email:</label>
+                <label className='float-start' htmlFor="email">Email:
+                    <span className={validateEmail() ? 'text-success' : 'text-danger'}> Must have valid email address</span>
+                </label>
                 <input className='form-control'
                 onChange={(e) => handleInput(e)} value={inputs.email}
                 type="text" name="email" id="email" />
             </div>
             <div className='my-3'>
-                <label className='float-start' htmlFor="password">Password:</label>
+                <label className='float-start' htmlFor="password">Password:
+                    <span className={validatePassword() ? 'text-success' : 'text-danger'}> Must have valid email address</span>
+                </label>
                 <input className='form-control'
                 onChange={(e) => handleInput(e)} value={inputs.password}
                 type="text" name="password" id="password" />
             </div>
             <div className='my-3'>
-                <label className='float-start' htmlFor="confirm">Confirm:</label>
+                <label className='float-start' htmlFor="confirm">Confirm:
+                    <span className={validateConfirm() ? 'text-success' : 'text-danger'}>Passwords must match</span>
+                </label>
                 <input className='form-control'
                 onChange={(e) => handleInput(e)} value={inputs.confirm}
                 type="text" name="confirm" id="confirm" />
             </div>
-            <input className='btn btn-dark' type="submit" value="Register" />
+            <input disabled={!canSubmit()} className='btn btn-dark' type="submit" value="Register" />
         </form>
     );
 }
