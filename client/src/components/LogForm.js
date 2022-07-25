d717de2f9ca36d799b271c87840851c30706eb8d
daleymike <98833177+daleymike@users.noreply.github.com>
2022-07-25 18:19:37 -0400

    added recipe list to log form, able to set total calories and recipe list on click

--- client/src/components/LogForm.js
+++ client/src/components/LogForm.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const LogForm = () => {
     const [logDate, setLogDate] = useState("");
     const [logRecipes, setLogRecipes] = useState([]);
+    const [logRecipeNames, setLogRecipeNames] = useState([]);
     const [logCalories, setLogCalories] = useState(0);
     const today = new Date();
+    const [allRecipes, setAllRecipes] = useState([]);
     
     
 
@@ -24,17 +27,47 @@ const LogForm = () => {
         }
     }
 
+    const handleClick = (e, recipe) => {
+        e.preventDefault()
+        setLogRecipeNames([...logRecipeNames, recipe.name]);
+        const totalCals = (logCalories + recipe.calories)
+        setLogCalories(totalCals);
+        // store recipe names and recipe ids seperate in state, push the ids through on submit
+    }
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/recipes")
+        .then((res) => {
+            console.log(res.data.recipes)
+            setAllRecipes(res.data.recipes)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    },[])
+
     return (
-        <div className='container'>
+        <div className='container d-flex'>
+            <div className="container w-75">
+            <h3 className='mt-5'>Recipes</h3>
+            <p>Click to Add Recipe to Log</p>
+            <ul className='list-group'>
+                {allRecipes.map((recipe, index) => {
+                   return( <li key={index} className='list-group-item' onClick={(e) => handleClick(e, recipe)} >{recipe.name} </li>)
+                })}
+                
+            </ul>
+            </div>
+            <div className="container">
             <h3 className='mt-5 mb-3'>Create Log</h3>
-            <form className='form m-auto' onSubmit={(e) => handleSubmit(e)}>
+            <form className='form m-auto w-75' onSubmit={(e) => handleSubmit(e)}>
                 <div className='form-group m-auto mt-2 mb-3'>
                     <label htmlFor="logDate">Date of Log: <span className={validateLogDate() ? 'text-success' : 'text-danger'} >Date cannot be in the future</span></label>
                     <input className='form-control' type="date" value={logDate} onChange={(e) => setLogDate(e.target.value)}/>
                 </div>
                 <div className='form-group m-auto mt-2 mb-3'>
                     <label htmlFor="logRecipes">Recipes: <span className={validateLogRecipes() ? 'text-success' : 'text-danger'} >Recipes are Required</span></label>
-                    <input className='form-control' type="text" value={logRecipes} onChange={(e) => setLogRecipes(e.target.value)}/>
+                    <input className='form-control' type="text" value={logRecipeNames} onChange={(e) => setLogRecipes(e.target.value)}/>
                 </div>
                 <div className='form-group m-auto mt-2 mb-3'>
                     <label htmlFor="logCalories">Total Caloires: <span className={validateLogCalories() ? 'text-success' : 'text-danger'} >Calories are required</span></label>
@@ -43,10 +76,8 @@ const LogForm = () => {
                 <div>
                     <button style={{width: 200, marginTop: 30}} className='btn btn-outline-dark form-control' type='submit'>Submit Log</button>
                 </div>
-            
-
             </form>
-
+            </div>
         </div>
     )
 }
