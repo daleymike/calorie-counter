4acdb88a04bd20e718d3e0f224890fcf887264d1
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-07-25 11:34:28 -0400

    Added the log CRUD backend

---
+++ server/models/log.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const LogSchema = new mongoose.Schema(
+    {
+        recipesEaten :{
+            type: String,
+            required: [true, "Recipes Eaten are required"],
+            minLength: [3, "Recipes Eaten must be at least 3 characters long"]
+        },
+        caloriesEaten :{
+            type: Number,
+            required: [true, "Calories Eaten are required"],
+            min: [0, "Calories Eaten must be have at least 0 calories"],
+    },
+        createdAt : {
+            type : Date,
+            required : true,
+            default : new Date(),
+            immutable : true
+    },
+
+        updatedAt : {
+            type : Date,
+            required : true,
+            default : new Date(),
+            immutable : true
+    }
+
+
+});
+
+
+module.exports = mongoose.model("Log", LogSchema);
