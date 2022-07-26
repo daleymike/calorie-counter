617ee39c3882208f69fd75986185abca65b9f40f
Kirkland Childers <robert.kirkland.childers@gmail.com>
2022-07-26 14:05:51 -0400

    Made necessary changes kc

--- server/models/log.model.js
+++ server/models/log.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const LogSchema = new mongoose.Schema(
     {
         recipesEaten :{
-            type: String,
+            type: [mongoose.Schema.Types.ObjectId],
             required: [true, "Recipes Eaten are required"],
             minLength: [3, "Recipes Eaten must be at least 3 characters long"]
         },
@@ -11,22 +11,24 @@ const LogSchema = new mongoose.Schema(
             type: Number,
             required: [true, "Calories Eaten are required"],
             min: [0, "Calories Eaten must be have at least 0 calories"],
-    },
+        },
+        logDate : {
+            type : Date,
+            required : true,
+            default : new Date()
+        },
         createdAt : {
             type : Date,
             required : true,
             default : new Date(),
             immutable : true
-    },
-
+        },
         updatedAt : {
             type : Date,
             required : true,
             default : new Date(),
             immutable : true
-    }
-
-
+        }
 });
 
 
