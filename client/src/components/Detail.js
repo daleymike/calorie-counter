a002a65b1c3c72ab8fcc45d2d551f2568589d8b8
Adrian Goyco <adriangoyco@gmail.com>
2022-07-26 22:56:46 -0400

    actual update

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -24,7 +24,7 @@ const Detail = (props) => {
 
 
 
-    const deleteRecipe = (idFromBelow) => {
+    const deleteRecipe = (recipeId) => {
         
         axios.delete(`http://localhost:8000/api/recipes/${recipeId}`)
         .then((res)=> {
